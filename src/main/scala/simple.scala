b197bcf4aa0b7034ca135d176fe61a9f9a1d9b71
Djoe Pramono <djoepramono@gmail.com>
2018-08-06 23:44:24 +1000

    Reworked simple sample

--- src/main/scala/simple.scala
+++ src/main/scala/simple.scala
@@ -1,21 +1,29 @@
 package com.studikode.studicirce
 
 import io.circe.parser.decode
-import io.circe.generic.semiauto._
+import io.circe.generic.semiauto.deriveDecoder
 
+case class Staff(name: String)
 
 object SimpleDecoder {
   def main(args: Array[String]): Unit = {
+
     val input =
       """
         {
           "name": "John Doe"
         }
-      """
+      """.stripMargin
 
-    implicit val librarianDecoder = deriveDecoder[Librarian]
-    val librarian = decode[Librarian](input)
+    // Using the derived encoder from Circe,
+    // we can just pass the case class as type parameter
+    // Yes, you need the implicit function
+    implicit val staffDecoder = deriveDecoder[Staff]
+    val decodeResult = decode[Staff](input)
 
-    println(librarian)
+    decodeResult match {
+      case Right(staff) => println(staff.name)
+      case Left(error) => println(error.getMessage())
+    }
   }
 }
