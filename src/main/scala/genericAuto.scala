4653a9211589be489544c6ce17724e6c01a01275
Djoe Pramono <djoepramono@gmail.com>
2018-08-08 23:38:44 +1000

    Add generic auto

---
+++ src/main/scala/genericAuto.scala
@@ -0,0 +1,32 @@
+package com.studikode.studicirce
+
+import io.circe.parser
+import io.circe.HCursor
+import io.circe.Decoder
+import io.circe.generic.auto._
+
+case class Applicant(firstName: String, lastName: Option[String])
+
+object OptionDecoder {
+  def main(args: Array[String]): Unit = {
+    val inputString =
+      """
+      [
+        {
+          "firstName": "Mary",
+          "lastName": "Jane"
+        },
+        {
+          "firstName": "Peter"
+        }
+      ]
+      """.stripMargin
+
+    val decodingResult = parser.decode[List[Applicant]](inputString)
+
+    decodingResult match {
+      case Right(applicants) => applicants.map(println)
+      case Left(error) => println(error.getMessage())
+    }
+  }
+}
